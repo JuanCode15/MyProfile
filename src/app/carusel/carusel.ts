b93a7940553f0a27c4a19a4128b843ac2a04291e
Sebastian <juanscampo15@gmail.com>
2025-12-07 03:30:07 -0500

    UpdatedeCarusel

--- src/app/carusel/carusel.ts
+++ src/app/carusel/carusel.ts
@@ -11,36 +11,7 @@ import { GalleriaModule } from 'primeng/galleria';
   styleUrls: ['./carusel.css'],
 })
 export class Carusel {
-  images: any[] = [
-    {
-      itemImageSrc: 'assets/img/hola.png',
-      thumbnailImageSrc: 'assets/img/aws.png',
-      alt: 'Descripción 1',
-      title: 'Título 1'
-    },
-    {
-      itemImageSrc: 'assets/img/aws.png',
-      thumbnailImageSrc: 'assets/img/aws.png',
-      alt: 'Descripción 2',
-      title: 'Título 2'
-    },
-    // ... más imágenes 
-  ];
-
-  responsiveOptions: any[] = [
-    {
-      breakpoint: '1024px',
-      numVisible: 5
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 3
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1
-    }
-  ];
+ 
 
 
 }
--- src/app/principal/principal.ts
+++ src/app/principal/principal.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { UiModule } from '../ui/ui-module';
 import { Header } from '../shared/header/header';
+import { Carusel } from '../carusel/carusel';
 
 
 
 @Component({
   selector: 'app-principal',
-  imports: [UiModule,Header],
+  imports: [UiModule,Header,Carusel],
   standalone: true,
   templateUrl: './principal.html', 
   styleUrl: './principal.css',
